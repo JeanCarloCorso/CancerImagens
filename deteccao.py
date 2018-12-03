a8dd42c6dbc0011ce56ea82a5bda775a19901689
JeanCarloCorso <jeancarlocorso@hotmail.com>
2018-12-03 11:17:29 -0200

    melhorando codigo

--- deteccao.py
+++ deteccao.py
@@ -4,22 +4,33 @@ import cv2
 import json
 import re
 
-#from os.path import join
-imagens = []
-tipo = []
-caminho = 'UDA-1'
-for (dirname, dirs, files) in os.walk(caminho):
-    for filename in files:
-        if filename.endswith('.jpg'):
-           imagens.append(cv2.imread(dirname+"//"+filename,cv2.IMREAD_ANYCOLOR))
-        if filename.endswith('.json'):
-            a = open(dirname+"//"+filename).read()
-            descricao = json.loads(a)
-            tipo.append(descricao['meta']['clinical']['benign_malignant'])
+def pegadados(caminho = 'UDA-1'):
+    imagens = []
+    tipo = []
+    for (dirname, dirs, files) in os.walk(caminho):
+        for filename in files:
+            if filename.endswith('.jpg'):
+               imagens.append(cv2.imread(dirname+"//"+filename,cv2.IMREAD_ANYCOLOR))
+            if filename.endswith('.json'):
+                a = open(dirname+"//"+filename).read()
+                descricao = json.loads(a)
+                tipo.append(descricao['meta']['clinical']['benign_malignant'])
+    return imagens, tipo
             
+def main():
+    imagens, label = pegadados("UDA-TESTE")
 
-for t in tipo:
-        print(t)
-cv2.imshow("imagen",imagens[0])
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    for i in label:
+        print(i)
+    
+    for imagen in imagens:
+        cv2.imshow("imagen",imagen)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+main()
+
+
+#cv2.imshow("imagen",imagens[0])
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
