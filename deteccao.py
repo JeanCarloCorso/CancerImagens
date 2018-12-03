24d50e1bb5269014d24faa316150862d12cf6476
JeanCarloCorso <jeancarlocorso@hotmail.com>
2018-12-03 15:49:22 -0200

    criando modelo da rede

--- deteccao.py
+++ deteccao.py
@@ -4,6 +4,13 @@ import cv2
 import json
 import re
 
+from keras.models import Sequential
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+
 def pegadados(caminho = 'UDA-1'):
     imagens = []
     tipo = []
@@ -17,6 +24,26 @@ def pegadados(caminho = 'UDA-1'):
                 tipo.append(descricao['meta']['clinical']['benign_malignant'])
     return imagens, tipo
 
+def CNN(largura, altura, canais, classes):
+    tamanho_entrada = (altura, largura, canais)
+ 
+    modelo = Sequential()
+    modelo.add(Conv2D(6, (5, 5), padding="same", input_shape=tamanho_entrada))
+    modelo.add(Activation("relu"))
+    modelo.add(MaxPooling2D((2,2)))
+    modelo.add(Conv2D(16, (5, 5)))
+    modelo.add(Activation("relu"))
+    modelo.add(MaxPooling2D((2,2)))
+    modelo.add(Flatten())
+    modelo.add(Dense(120))
+    modelo.add(Activation("relu"))
+    modelo.add(Dense(84))
+    modelo.add(Activation("relu"))
+    modelo.add(Dense(classes))
+    modelo.add(Activation("softmax"))
+ 
+    return modelo
+
 def divide(img, labels):
     tamanho_teste = int(len(img)//3)
 
@@ -25,7 +52,6 @@ def divide(img, labels):
     treino_img = []
     treino_labels = []
 
-    print(tamanho_teste)
     for i in range(0,len(img)):
         if i < tamanho_teste:
             teste_img.append(img[i])
@@ -39,15 +65,14 @@ def divide(img, labels):
 def main():
     imagens, label = pegadados("UDA-TESTE")
     teste_img, treino_img, teste_labels, treino_labels = divide(imagens, label)
-    print("---teste---\n", teste_labels, "\n---treino---\n", treino_labels)
-
-    for i in label:
-        print(i)
-    
+    largura, altura, canais = imagens[0].shape
+    cnn = CNN(largura,altura,canais,2)
+    """
     for imagen in imagens:
         cv2.imshow("imagen",imagen)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+    """
 
 main()
 
