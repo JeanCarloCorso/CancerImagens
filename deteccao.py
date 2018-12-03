b6cd22b02d54cab9aa3c910539b9851abf4663af
JeanCarloCorso <jeancarlocorso@hotmail.com>
2018-12-03 11:41:31 -0200

    divisão dados

--- deteccao.py
+++ deteccao.py
@@ -16,9 +16,20 @@ def pegadados(caminho = 'UDA-1'):
                 descricao = json.loads(a)
                 tipo.append(descricao['meta']['clinical']['benign_malignant'])
     return imagens, tipo
-            
+
+def divide(img, labels):
+    tamanho_teste = len(img)/3
+    img_teste = img[0:tamanho_teste,...]
+    img_treino = img[tamanho_teste:,...]
+
+    labels_teste = labels[0:labels,...]
+    labels_treino = labels[labels:,...]
+
+    return img_teste, img_treino, labels_teste, labels_treino 
+
 def main():
     imagens, label = pegadados("UDA-TESTE")
+    img_teste, img_treino, labels_teste, labels_treino = divide(imagens, label)
 
     for i in label:
         print(i)
